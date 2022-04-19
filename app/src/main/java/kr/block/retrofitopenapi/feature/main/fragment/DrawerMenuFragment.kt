07d8f13c435a8518114ade866e3fecd151b0a721
hschoe <chldd12@gmail.com>
2022-04-19 17:11:10 +0900

    ECT : 일부 누락된 파일 PUSH

--- app/src/main/java/kr/block/retrofitopenapi/feature/main/fragment/DrawerMenuFragment.kt
+++ app/src/main/java/kr/block/retrofitopenapi/feature/main/fragment/DrawerMenuFragment.kt
@@ -2,18 +2,38 @@ package kr.block.retrofitopenapi.feature.main.fragment
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kr.block.retrofitopenapi.R
 import kr.block.retrofitopenapi.databinding.FragmentMainDrawerMenuBinding
 import kr.block.retrofitopenapi.feature.common.fragment.BaseFragment
 
-class DrawerMenuFragment : BaseFragment<FragmentMainDrawerMenuBinding>(R.layout.fragment_main_drawer_menu) {
+class DrawerMenuFragment :
+    BaseFragment<FragmentMainDrawerMenuBinding>(R.layout.fragment_main_drawer_menu),
+    View.OnClickListener {
 
     companion object {
-        fun newInstance():Fragment = DrawerMenuFragment()
+        fun newInstance(): Fragment = DrawerMenuFragment()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        with(mBinder) {
+            menuProfile.rootView.setOnClickListener(this@DrawerMenuFragment)
+            menuDefaultSet.rootView.setOnClickListener(this@DrawerMenuFragment)
+        }
+    }
+
+    override fun onClick(view: View?) {
+        with(mBinder) {
+            when (view) {
+                menuProfile.rootView -> {
+                    Toast.makeText(context, "프로필", Toast.LENGTH_LONG).show()
+                }
+                menuDefaultSet.rootView -> {
+                    Toast.makeText(context, "기본세팅", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 }
